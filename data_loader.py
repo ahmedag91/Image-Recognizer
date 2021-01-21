713d7fee12829573fd5c5d6479409605c70124a2
ahmedag91 <ahmed.abdulgawad91@gmail.com>
2021-01-21 01:18:24 +0100

    feat: Update data_loader.py
    Add load_image_dataset function to load train, validation, and testing sets given a folder specifying the load directory of a dataset

---
+++ data_loader.py
@@ -0,0 +1,32 @@
+
+from torchvision import datasets, transforms
+import torch 
+def load_image_dataset(dataset_dir: str):
+    
+    train_dir = dataset_dir + '/train'
+    valid_dir = dataset_dir + '/valid'
+    test_dir = dataset_dir + '/test'
+    mean_per_channel, std_per_channel = (0.485, 0.456, 0.406), (0.229, 0.224, 0.225)
+
+    data_transforms = transforms.Compose([
+                                        transforms.RandomResizedCrop(224), 
+                                        transforms.ToTensor(),
+                                        transforms.Normalize(mean_per_channel, std_per_channel)
+                                    ])
+                                    
+    image_datasets = dict()
+    image_datasets['train'] = datasets.ImageFolder(train_dir, transform=data_transforms)
+    image_datasets['valid'] = datasets.ImageFolder(valid_dir, transform=data_transforms)
+    image_datasets['test'] = datasets.ImageFolder(test_dir, transform=data_transforms)
+
+
+    dataloaders = dict()
+    dataloaders['train'] = torch.utils.data.DataLoader(image_datasets['train'], batch_size=64, shuffle=True)
+    dataloaders['valid'] = torch.utils.data.DataLoader(image_datasets['valid'], batch_size=64, shuffle=True)
+    dataloaders['test'] = torch.utils.data.DataLoader(image_datasets['test'], batch_size=64, shuffle=True)
+    return [dataloaders['train'], dataloaders['valid'], dataloaders['test']]
+
+
+#switcher = {'one': 1, 'two': 2}
+#print(switcher.get('one', 'invalid number'))
+
