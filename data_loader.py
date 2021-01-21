9764cbdcc9c5cf275456943ee496ec8c4b1ac56d
ahmedag91 <ahmed.abdulgawad91@gmail.com>
2021-01-21 14:04:26 +0100

    feat: Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -3,6 +3,21 @@ from torchvision import datasets, transforms
 import torch 
 def load_image_dataset(dataset_dir: str):
     
+    dataset_dirs = [dataset_dir + '/train', dataset_dir + '/valid', dataset_dir + '/test']
+    mean_per_channel, std_per_channel = (0.485, 0.456, 0.406), (0.229, 0.224, 0.225)
+    crop_size = 224
+    data_transforms = transforms.Compose([
+                                        transforms.RandomResizedCrop(crop_size), 
+                                        transforms.ToTensor(),
+                                        transforms.Normalize(mean_per_channel, std_per_channel)
+                                    ])
+    data_set = []
+    for dir in dataset_dirs:
+        data_set.append(torch.utils.data.DataLoader(datasets.ImageFolder(dir, transform=data_transforms), batch_size=64, shuffle=True))
+        
+    return (data for data in data_set) 
+    """
+
     train_dir = dataset_dir + '/train'
     valid_dir = dataset_dir + '/valid'
     test_dir = dataset_dir + '/test'
@@ -25,6 +40,7 @@ def load_image_dataset(dataset_dir: str):
     dataloaders['valid'] = torch.utils.data.DataLoader(image_datasets['valid'], batch_size=64, shuffle=True)
     dataloaders['test'] = torch.utils.data.DataLoader(image_datasets['test'], batch_size=64, shuffle=True)
     return [dataloaders['train'], dataloaders['valid'], dataloaders['test']]
+    """
 
 
 #switcher = {'one': 1, 'two': 2}
